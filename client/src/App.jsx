680d689136362c306345e6e4d58bbdf038406655
Nur Akram <mnagamingchannel@gmail.com>
2026-06-01 15:19:13 +0000

    contact file added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Landing from "./pages/landing.jsx";
 import Login from "./pages/login.jsx";
 import Register from "./pages/register.jsx";
 import Navbar from "./pages/navbar.jsx";
+import Contact from "./pages/contact.jsx";
 import Leaderboard from "./pages/leaderboard.jsx";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/leaderboard" element={<Leaderboard />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </>
   );
